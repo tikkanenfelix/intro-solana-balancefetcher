53860f13cca79bfd330950d281acca9a3a2655c2
Felix T <79270989+tikkanenfelix@users.noreply.github.com>
2022-11-06 11:31:34 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+    try {
+      setAddress(address)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false)
+      })
+      })
+    } catch (error) {
+      setAddress('')
+      setBalance(0)
+      alert(error)
+    }
   }
+
+
 
   return (
     <div className={styles.App}>
@@ -23,6 +40,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable? 'Yp': 'Nope' }`}</p>
       </header>
     </div>
   )
